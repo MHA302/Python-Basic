6b3c035aa2279d45ea155243bed84ca7e94f259d
muddassarH <muddassarhussain90@gmail.com>
2023-05-05 00:22:47 +0500

    Adding New Items Through Web Feature

--- web.py
+++ web.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import functions
 
-
-
 todos = functions.get_todos()
 
 
@@ -16,9 +14,12 @@ st.title("My ToDO app")
 st.subheader("This is my todo app.")
 st.write("This Application is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
-
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.add_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 st.text_input(label=" ", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
-
