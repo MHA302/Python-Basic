17905ffea3190a4b19cedb552cc7278203eb7e50
muddassarH <muddassarhussain90@gmail.com>
2023-05-04 18:21:15 +0500

    Edit Complete Error's Removes and add a new Theme

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
 
 sg.theme("Black")
 clock_label = sg.Text('', key='clock')
